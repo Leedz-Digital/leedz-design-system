f76ae64781646d410e31f916f0d4d7fd1d72524d
Fernando Alves <fernando.advpl@gmail.com>
2024-07-13 01:08:59 -0300

    Ajuste Container

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+// src/index.js
 export { default as Button } from './components/Button/Button';
+export { default as Container } from './components/Container/Container';
 export { default as theme } from './styles/theme';
